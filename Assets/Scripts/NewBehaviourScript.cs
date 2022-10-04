ebe8762f2bef4c144526c853744f7dacdd982d6a
JustinG711 <111902925+JustinG711@users.noreply.github.com>
2022-10-03 23:44:53 -0400

    Animation commit

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -8,10 +8,12 @@ public class NewBehaviourScript : MonoBehaviour
     public AudioClip musicClipTwo;
     public AudioSource musicSource;
 
+    Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+      anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,25 +23,26 @@ public class NewBehaviourScript : MonoBehaviour
         {
           musicSource.clip = musicClipOne;
           musicSource.Play();
-
+          anim.SetInteger("State", 1);
          }
 
      if (Input.GetKeyUp(KeyCode.W))
         {
           musicSource.Stop();
-
+          anim.SetInteger("State", 0);
          }
 
      if (Input.GetKeyDown(KeyCode.R))
         {
           musicSource.clip = musicClipTwo;
           musicSource.Play();
+          anim.SetInteger("State", 2);
          }
 
      if (Input.GetKeyUp(KeyCode.R))
         {
           musicSource.Stop();
-
+          anim.SetInteger("State", 0);
          }
 
      if (Input.GetKeyDown(KeyCode.L))
